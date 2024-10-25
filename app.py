ad26c14ff8328c7d8ad6a4c4fb3a06b17772f4a7
Bilal KHan <130842665+BilalKhanlol@users.noreply.github.com>
2024-10-25 08:21:44 +0530

    added port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from gradio_client import Client
+import os
 
 app = Flask(__name__)
 client = Client("black-forest-labs/FLUX.1-schnell")
@@ -36,4 +37,5 @@ def infer():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
